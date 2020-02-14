adcf46928ef14a9ed7ffc6433138bb9c7101697d
Batfoxkid <batfoxkid@gmail.com>
2020-02-14 14:36:51 -0800

    Development Branch freak_fortress_2.inc

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,5 +14,5 @@ cd addons/sourcemod/scripting
 wget "https://www.doctormckay.com/download/scripting/include/morecolors.inc" -O include/morecolors.inc
 
 # Install Third-Parties
-wget "https://raw.githubusercontent.com/Batfoxkid/FreakFortressBat/master/addons/sourcemod/scripting/include/freak_fortress_2.inc" -O include/freak_fortress_2.inc
+wget "https://raw.githubusercontent.com/Batfoxkid/FreakFortressBat/development/addons/sourcemod/scripting/include/freak_fortress_2.inc" -O include/freak_fortress_2.inc
 wget "https://raw.githubusercontent.com/Totenfluch/tVip/master/include/tVip.inc" -O include/tVip.inc
