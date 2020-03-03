19cbd96eb504d2276d2248888c98ec61fa5c19af
Batfoxkid <batfoxkid@gmail.com>
2020-03-03 11:10:47 -0800

    Add Chat Processor to Install

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,5 +14,6 @@ cd addons/sourcemod/scripting
 wget "https://www.doctormckay.com/download/scripting/include/morecolors.inc" -O include/morecolors.inc
 
 # Install Third-Parties
+wget "https://raw.githubusercontent.com/Drixevel/Chat-Processor/master/scripting/include/chat-processor.inc" -O include/chat-processor.inc
 wget "https://raw.githubusercontent.com/Batfoxkid/FreakFortressBat/development/addons/sourcemod/scripting/include/freak_fortress_2.inc" -O include/freak_fortress_2.inc
 wget "https://raw.githubusercontent.com/Totenfluch/tVip/master/include/tVip.inc" -O include/tVip.inc
