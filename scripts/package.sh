d6e6c528fe9684732022fb81d3eed886bfcbf3b4
Nahuel <link73257@gmail.com>
2024-06-01 20:22:11 -0300

    Add MySQL to workflows

--- scripts/package.sh
+++ scripts/package.sh
@@ -11,5 +11,6 @@ cp -r addons/sourcemod/plugins/textstore.smx package/addons/sourcemod/plugins
 cp -r addons/sourcemod/plugins/textstore_defaults.smx package/addons/sourcemod/plugins/disabled
 cp -r addons/sourcemod/plugins/textstore_generic.smx package/addons/sourcemod/plugins/disabled
 cp -r addons/sourcemod/plugins/textstore_sqlite.smx package/addons/sourcemod/plugins/disabled
+cp -r addons/sourcemod/plugins/textstore_mysql.smx package/addons/sourcemod/plugins/disabled
 cp -r addons/sourcemod/plugins/textstore_tf2.smx package/addons/sourcemod/plugins/disabled
 cp -r ../addons/sourcemod/configs/textstore package/addons/sourcemod/configs
